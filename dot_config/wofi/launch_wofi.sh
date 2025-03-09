c1a8075e2255304e589673e2139395bd4ea7532e
lillian0x1a <lillian0x1a@gmail.com>
2025-03-09 14:11:59 +0900

    Initial commit

---
+++ dot_config/wofi/launch_wofi.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Script to launch Wofi with background blur
+
+# Adjust the following command to match your Wofi setup
+wofi_command="wofi --show drun"
+
+# Launch Wofi
+$wofi_command
