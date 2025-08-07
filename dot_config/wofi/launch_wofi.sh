f0c0c42287d33a35be3d3294a66bf34438ef79cb
lillian0x1a <183957662+lillian0x1a@users.noreply.github.com>
2025-08-07 05:03:13 +0000

    Delete dot_config/wofi directory

--- dot_config/wofi/launch_wofi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Script to launch Wofi with background blur
-
-# Adjust the following command to match your Wofi setup
-wofi_command="wofi --show drun"
-
-# Launch Wofi
-$wofi_command
