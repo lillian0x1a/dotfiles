fbcee2a4f0e7ce5288c45fc0e216bf44199697f3
lillian0x <183957662+lillian0x1a@users.noreply.github.com>
2025-10-19 12:24:12 +0900

    change ime from mozc to skk

--- home/ime.nix
+++ home/ime.nix
@@ -4,9 +4,9 @@
     type = "fcitx5";
     fcitx5 = {
       addons = with pkgs; [
-        fcitx5-mozc         # Mozc engine for Japanese input.
-        fcitx5-gtk          # GTK integration modules for Fcitx5.
-        fcitx5-nord
+        fcitx5-skk
+        fcitx5-gtk
+        # fcitx5-nord
       ];
       settings = {
         inputMethod = {
@@ -14,16 +14,15 @@
           "Groups/0" = {
             Name = "Default";
             "Default Layout" = "us";
-            DefaultIM = "mozc";
+            DefaultIM = "skk";
           };
-          "Groups/0/Items/0".Name = "keyboard-us";
-          "Groups/0/Items/1".Name = "mozc";
+          "Groups/0/Items/0".Name = "skk";
+          "Groups/0/Items/1".Name = "keyboard-us";
         };
       };
     };
   };
   wayland.windowManager.hyprland.settings.exec-once = [
-    # "fcitx5-remote -r"
     "fcitx5 -d --replace"
   ];
 }
