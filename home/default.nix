6732898b88e80d3fa4d3bf3e27bd10721a475c9f
lillian0x <183957662+lillian0x1a@users.noreply.github.com>
2025-10-19 10:19:26 +0900

    add nh

--- home/default.nix
+++ home/default.nix
@@ -31,5 +31,10 @@
       vscode
     ];
     programs.home-manager.enable = true;
+    programs.nh = {
+      enable = true;
+      clean.enable = true;
+      clean.extraArgs = "--keep-since 4d --keep 3";
+    };
   };
 }
